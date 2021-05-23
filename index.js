97c7c620c638bd78c178aceb60d09114877d358c
Kessler1459 <kessler1459@gmail.com>
2021-05-23 13:59:58 -0300

    rooms

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const {Server,Socket} = require("socket.io");
+const { Server, Socket } = require("socket.io");
+const { addUser, getUser, deleteUser, getUsers, getAll } = require("./users");
 
 const io = new Server(server, {
     cors: true,
@@ -10,9 +11,21 @@ const io = new Server(server, {
 });
 
 io.on("connection", (client) => {
-    client.send("conectado");
-    client.on("message",(msg) => {
-        io.emit("message",msg);
+    console.log("connection " + client.id);
+    client.on("login", ({ name, room }) => {
+        const { user, error } = addUser(client.id, name, room);
+        client.join(user.room);
+        client.in(room).emit("alert", { connected: true, user: user.name });
+        console.log("alert" + ` ${user.name} has joined the room ${user.room}`);
+        io.to(room).emit("users", getUsers(room));
+    });
+    client.on("message", (msg) => {
+        const u = getUser(client.id);
+        io.to(u.room).emit("message", { username: u.name, body: msg.body });
+    });
+    client.on("disconnect",()=>{
+        const u =getUser(client.id);
+        client.in(u.room).emit("alert", { connected: false, user: u.name });
     })
 });
 
---
+++ users.js
@@ -0,0 +1,26 @@
+const users = [];
+
+const addUser = (id, name, room) => {
+    const existingUser = users.find(
+        (user) => user.name.trim().toLowerCase() === name.trim().toLowerCase()
+    );
+    if (existingUser) return { error: "Username has already been taken" };
+    if (!name || !room) return { error: "Username and room are required" };
+    room=room+"";
+    const user = { id, name, room };
+    users.push(user);
+    return { user };
+};
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+const deleteUser = (id) => {
+    const index = users.findIndex((user) => user.id === id);
+    if (index !== -1) return users.splice(index, 1)[0];
+};
+
+const getUsers = (room) => users.filter((user) => user.room === room);
+
+const getAll=()=>users
+
+module.exports = { addUser, getUser, deleteUser, getUsers, getAll };
